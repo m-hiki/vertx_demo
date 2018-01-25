9e1698ca886056b374e14b37b771e3f429433286
Minoru Hiki <minoruhiki@gmail.com>
2018-01-25 19:37:26 +0900

    Add Logging library. I choose the SLF4J and Logback for the recommendation in the Vert.x guild.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # vertx_demo
 
 
+## Architecture
+- Language: Java 1.8.0_161
+- Build: Gradle 4.3.1
+- Base Framework: Vert.x 3.5.0
+- Logging: SLF4J + Logback
+
 ## Launch
 
-```
+```shell
 ./gradlew run
 ```
--- src/main/java/com/example/demo/MainVerticle.java
+++ src/main/java/com/example/demo/MainVerticle.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
 import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+    
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> {
+              LOGGER.info("Access!");
               req.response()
                 .putHeader("content-type", "text/plain")
                 .end("Hello from Vert.x!");
