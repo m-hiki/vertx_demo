1b0cdb28bf8b076887bd24b058faf4b574982906
Minoru Hiki <minoruhiki@gmail.com>
2018-01-26 21:05:35 +0900

    Add template engine example

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
 # vertx_demo
 
 
+## Launch
+
+```shell
+./gradlew run
+```
+
 ## Architecture
 - Language: Java 1.8.0_161
 - Build: Gradle 4.3.1
 - Base Framework: Vert.x 3.5.0
 - Logging: SLF4J + Logback
 
-## Launch
+### Design
+- Definitions
+- Domain
+    - Package: (project).(product).domain
+    - Class Type: Entity, DTO, VO
+        - Entity: the object corresponding to DB Object
+        - DTO: the object to transfer across networks or process.
+        - VO: Immutable object
+- Repository
+    - Package: (project).(product).repository
+    - Class Type: Cache, DAO
+        - Cache : 
+        - DAO :
+- Service
+    - Package: (project).(product).service
+    - Class Type: Worker,
+- Controller
 
-```shell
-./gradlew run
-```
+
+- Verticle
+    - 
+    - 
--- src/main/java/com/example/demo/MainVerticle.java
+++ src/main/java/com/example/demo/MainVerticle.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class MainVerticle extends AbstractVerticle {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+    private static final int PORT = 8081;
     
+    /*
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> {
@@ -16,5 +21,36 @@ public class MainVerticle extends AbstractVerticle {
                 .end("Hello from Vert.x!");
             }).listen(8080);
         System.out.println("HTTP server started on port 8080");
+    }
+    */
+
+    // ThymeleafTemplateEnging
+    @Override
+    public void start() throws Exception {
+        // To simplify the development of the web components we use a Router to route all HTTP requests
+        // to organize our code in a reusable way.
+        final Router router = Router.router(vertx);
+
+        // In order to use a Thymeleaf template we first need to create an engine
+        final ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
+
+        router.get().handler(ctx -> {
+            // we define a hardcoded title for our application
+            ctx.put("welcome", "Hi there!");
+
+            // and now delegate to the engine to render it.
+            engine.render(ctx, "templates/index.html", res -> {
+                if (res.succeeded()) {
+                    LOGGER.info("Access!");
+                    ctx.response().end(res.result());
+                } else {
+                    ctx.fail(res.cause());
+                }
+            });
+        });
+
+        // start a HTTP web server on port 8080
+        vertx.createHttpServer().requestHandler(router::accept).listen(PORT);
+        LOGGER.info("HTTP server started on port " + PORT);
     }
 }
